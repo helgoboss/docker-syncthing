88fac8f0753998037b2f5aa01b80de78bd16e9a2
Benjamin Klum <benjamin.klum@helgoboss.org>
2018-02-04 21:04:55 +0100

    Remove Rancher-specific stuff, need to set this now manually

--- start.sh
+++ start.sh
@@ -7,10 +7,6 @@ if [ ! -f "$CONFIG_FILE" ]; then
     syncthing -generate="$CONFIG_FOLDER"
     export LOCAL_DEVICE_ID=`xmlstarlet select --template --value-of /configuration/device/@id $CONFIG_FILE`
     export GUI_API_KEY=`xmlstarlet select --template --value-of /configuration/gui/apikey $CONFIG_FILE`
-    : ${DEVICE_NAME:=`curl http://rancher-metadata/2015-12-19/self/host/hostname`}
-    : ${DEVICE_IP:=`curl http://rancher-metadata/2015-12-19/self/host/agent_ip`}
-    export DEVICE_NAME
-    export DEVICE_IP
     confd -onetime -backend env
 fi
 
